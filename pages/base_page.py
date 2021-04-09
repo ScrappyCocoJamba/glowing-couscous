a17e3d29956527bc108883c051a05de108b20bac
ScrappyCocoJamba <konstantinmart10@gmail.com>
2021-04-09 12:59:48 +0700

    test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,1 +1,12 @@
-pass
+from .locators import BasePageLocators
+from selenium.common.exceptions import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage():
+
+    def __init__(self, browser, url, timeout=10):  #конструктор — метод, который вызывается, когда мы создаем объект
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
