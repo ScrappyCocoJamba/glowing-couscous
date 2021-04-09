be31f9f38a479325f21215602a046e817f966e4c
ScrappyCocoJamba <konstantinmart10@gmail.com>
2021-04-09 16:31:33 +0700

    add

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,9 @@ from .pages.main_page import MainPage
 import pytest
 
 # pytest -v --tb=line --language=en test_main_page.py::test_guest_cant_see_product_in_basket_opened_from_main_page
-# pytest -v --tb=line --language=en -m login_guest test_main_page.py
+# pytest -v --tb=line --language=en --browser_name=chrome -m login_guest test_main_page.py
 
 url = "http://mail.ru/"
-
 
 
 @pytest.mark.login_guest
@@ -14,5 +13,5 @@ class TestLoginFromMainPage():
 
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, url)          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                             # открываем страницу
+        page.open()                            # открываем страницу
         time.sleep(10)
